98316e5d5a761b2b7267a37efc5ff95545493546
Joon Ho Cho <joonho1101@gmail.com>
2023-03-15 22:33:30 +0900

    jest test

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
-  resolver: 'jest-ts-webcompat-resolver',
+  preset: 'ts-jest/presets/js-with-babel', // needed to import es module library in ts test files
+  resolver: 'jest-ts-webcompat-resolver', // needed to import ts files with js extension in ts test files
   testEnvironment: 'node',
+  transformIgnorePatterns: ['node_modules/(?!@joonhocho|util-3gcvv)'], // needed to import es module library in ts test files
 };
 
 export default config;
